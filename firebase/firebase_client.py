1e61326d705fb0c36dd07cfd58cc41bf860fa02a
Abdelhady <shalsh902@gmail.com>
2025-02-03 17:48:44 +0300

    update code to deploy on vercel

--- firebase/firebase_client.py
+++ firebase/firebase_client.py
@@ -1,20 +1,16 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-from dotenv import load_dotenv
-import os
 
-# Load environment variables from .env file
-load_dotenv()
 
 def initialize_firebase():
-    cred_path = os.getenv("FIREBASE_CREDENTIALS_PATH")
+
     if not firebase_admin._apps:  # Check if Firebase is already initialized
-        cred = credentials.Certificate(cred_path)
+        cred = credentials.Certificate("thebazar-serviceAccount.json")
         firebase_admin.initialize_app(cred)
         print("Firebase initialized successfully!")
     else:
         print("Firebase already initialized.")
-    
+
 initialize_firebase()
 
 def get_products():
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI, HTTPException
-import webbrowser
 from services.recommendation import get_recommendations
 from models.product import Product
-from firebase.firebase_client import get_products  # This will initialize Firebase
+from firebase.firebase_client import get_products 
 
 
 app = FastAPI()
 
-# Default product ID
-DEFAULT_PRODUCT_ID = "GpdyzYCYEMcmpr02fTOV"
 
 @app.get("/recommendations/{product_id}", response_model=list[Product])
 async def get_recommendations_api(product_id: str):
@@ -20,16 +17,7 @@ async def get_recommendations_api(product_id: str):
         raise HTTPException(status_code=404, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-        
-def open_browser_with_recommendation(product_id: str):
-    """Open the browser with a Firebase link containing the recommendation"""
-    # Construct the Firebase URL
-    firebase_url = f"http://localhost:8000/recommendations/{product_id}"
-    
-    # Open the URL in the default browser
-    webbrowser.open(firebase_url)
+
 if __name__ == "__main__":
     import uvicorn
-   
-    open_browser_with_recommendation(DEFAULT_PRODUCT_ID)
     uvicorn.run(app, host="0.0.0.0", port=8000)
